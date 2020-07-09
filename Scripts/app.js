498bcc822c484094504237feb985be6b8dfb3cb0
fatiheroltr <fatiheroltr@gmail.com>
2020-07-08 22:44:49 -0400

    Rebuild

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,7 +6,7 @@
 
     let StringVariables = {
       introParagraph:
-        "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.</br></br>Greece has 1,400 islands, though only 230 of them are inhabited.</br></br>And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.",
+        "FATIH - Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.</br></br>Greece has 1,400 islands, though only 230 of them are inhabited.</br></br>And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.",
       folegandros:
         "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.",
       alonissos:
